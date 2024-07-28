3c314224ac341f8c9b7433649413d593da953d3c
Saurabh <71336366+Speedyboy001@users.noreply.github.com>
2024-07-28 18:16:33 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,80 +1,35 @@
 /*
- * main.c
+ * GccApplication2.c
  *
- * Created: 7/25/2024 10:24:05 PM
- *  Author: hp
+ * Created: 28-07-2024 10.27.43 AM
+ * Author : Speedyboy
  */ 
-#include <util/delay.h>
-#include <stdint.h>
+#define F_CPU 16000000UL // Define CPU frequency as 16 MHz
+
 #include <avr/io.h>
-// #include <xc.h>
+#include <util/delay.h>
 
-//PORTB
-#define  BaseADDR	0x25
-#define  PinADDR	0x23
-#define  DrrADDR	0x24
-#define	 PinNumber	5
-
-//PORTC
-// #define  BaseADDRC	0x28
-// #define  PinADDRC	0x26
-// #define  DrrADDRC	0x27
-// #define	 PinNumberC	4
-#define  PinPress   0
-void Delay(unsigned int count);
+#define LedPin 4
+#define ButtonPin 0
 int main(void)
 {
-	uint8_t *ledport = (uint8_t*)BaseADDR;
-	uint8_t *ledpin = (uint8_t*)PinADDR;
-	uint8_t *leddrr = (uint8_t*)DrrADDR;
-	/*
-	uint8_t *ledportC = (uint8_t*)BaseADDRC;
-	uint8_t *ledpinC = (uint8_t*)PinADDRC;
-	uint8_t *leddrrC = (uint8_t*)DrrADDRC;
-	*/
+    DDRB |= (1<<LedPin); //led pin direction as an output
+	DDRB &= ~(1<<ButtonPin); //Button pin direction as an input 
 	
-	*ledport	|=	(1<<PinNumber);
-	*ledport	|=	(1<<PinPress);
-	*ledpin		|=	(1<<PinNumber);
-	*ledpin		&=  ~(1<<PinPress);
-	*leddrr		&=  ~(1<<PinPress);
-	*leddrr		|=  (1<<PinNumber);
+	PORTB |= (1<<ButtonPin);
 	
-	
-	
-	// press button
-	
-/*
-// 	*ledport	|=	(1<<PinNumber);
-// 	*ledpin		|=	(1<<PinNumber);
-// 	*leddrr		|=  (1<<PinNumber);
-*/
-	
-	uint8_t var = 0x00000001;
-    while(1)
+    while (1) 
     {
-        //TODO:: Please write your application code 
-// 		if (var & (*ledpin |= (1<<PinPress)))
-// 		{
-// 			_delay_ms(100);
-// 			*ledpin		|=	(1<<PinNumber);
-// 		}
-	/*	*ledpin		&=	~(1<<PinNumber);*/
-	*ledpin		|=	(1<<PinNumber);
-	Delay(1000);
-	*ledpin		&=	~(1<<PinNumber);
-	Delay(1000);
-	
-	
+		if (!(PINB & (1<<ButtonPin)))
+		{
+			
+			PORTB &= ~(1<<LedPin);
+		}
+		else
+		{
+			PORTB |= (1<<LedPin);
+		}
     }
+	return 0;
 }
-void Delay(unsigned int count)
-{
-	while (count--)
-	{
-		for (volatile unsigned int i = 0; i < count; i++) {
-			// Busy-wait loop
-			}
-		
-	}
-}
+
